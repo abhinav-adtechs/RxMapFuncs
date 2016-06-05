1484f641967233ab621be768968440deb3dbc2c2
abhinav-adtechs <abhinav.adtechs@gmail.com>
2016-06-05 13:01:40 +0530

    Initial commit

---
+++ app/src/main/java/abhinav/hackdev/co/fetchmarker102/Controller.java
@@ -0,0 +1,64 @@
+package abhinav.hackdev.co.fetchmarker102;
+
+import android.app.Application;
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by abhinav on 05/06/16.
+ */
+public class Controller extends Application{
+
+    public static final String TAG = Controller.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+
+    private static Controller mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized Controller getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public Context getAppContext(){
+        return getApplicationContext() ;
+    }
+
+    public static Context getStaticAppContext(){
+        return Controller.getInstance().getAppContext() ;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+
+}
